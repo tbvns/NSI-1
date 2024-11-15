b22265ef5fbcb17273b33844aca5ee5aa661b827
noenabon <120752984+noenabon@users.noreply.github.com>
2024-11-15 11:49:46 +0100

    Update ggez-tp5.py Ex2

--- ggez-tp5.py
+++ ggez-tp5.py
@@ -1,6 +1,3 @@
-from tensorflow.compiler.tf2xla.python.xla import ndtri
-
-
 def ex1():
     text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     d = {}
@@ -12,6 +9,16 @@ def ex1():
         d[s]=1
         l.append([s, d[s]])
 
+def ex2():
+    result = []
+    for i in range(1000):
+        somme = 0
+        for x in str(i):
+            somme += int(x)**3
+        if int(i) == somme:
+            result.append(i)
+    return result
+print(ex2())
 
 def ex3():
     a ="abcdefghijklmnopqrstuvwxyz"
