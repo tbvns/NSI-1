15d95aa0b59444114c707f119143c6a4fc5cd6f1
dev <test@gmail.com>
2024-11-18 09:46:18 +0100

    tp6 ex2

--- hoho-tp6.py
+++ hoho-tp6.py
@@ -1,1 +1,9 @@
-def ex1():
+def ex3():
+    s = "&"
+    false = False
+    while false:
+        for cha in s:
+            if not cha.isalpha() and not cha.isdigit():
+                s = input("saisie en minuscules, majuscules et chiffres seulement : ")
+            else:
+                false = True
